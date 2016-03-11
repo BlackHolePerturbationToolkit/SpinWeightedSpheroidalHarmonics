e1fecfe04c548d79e459c5c205fe41e423290580
Barry Wardell <barry.wardell@gmail.com>
2016-03-11 08:30:21 +0000

    Add basic package files

---
+++ PacletInfo.m
@@ -0,0 +1,19 @@
+Paclet[
+  Name -> "SpinWeightedSpheroidalHarmonics",
+  Version -> "0.1.0",
+  MathematicaVersion -> "8+",
+  Creator -> "Barry Wardell",
+  Description -> "A set of functions for computing spin-weighted spheroidal harmonics.",
+  Extensions -> {
+    { "Kernel",
+	  "Context" -> {"SpinWeightedSpheroidalHarmonics`"}
+	},
+
+    {"Documentation",
+     Language -> "English", 
+     MainPage -> "Guides/SpinWeightedSpheroidalHarmonics",
+     Resources -> 
+     	{"Guides/SpinWeightedSpheroidalHarmonics"}
+    }
+  }
+]
